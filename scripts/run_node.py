9399eea5e8b7ccee4592928870e1a024027e666a
Yusuboy <Yuusufab@outlook.com>
2025-12-05 16:12:08 +0200

    update script

--- scripts/run_node.py
+++ scripts/run_node.py
@@ -2,10 +2,20 @@ import asyncio
 import sys
 from src.core.node import Node
 
-if __name__ == "__main__":
+async def main():
     node_id = sys.argv[1]
     port = int(sys.argv[2])
     peers = sys.argv[3].split(",") if len(sys.argv) > 3 else []
 
     node = Node(node_id=node_id, host="127.0.0.1", port=port, peers=peers)
-    asyncio.run(node.start())
+
+    # Force A to act as leader for demo
+    if node_id == "A":
+        node.role = "leader"
+        node.term = 1
+        asyncio.create_task(node.leader_heartbeat_loop())
+
+    await node.start()
+
+if __name__ == "__main__":
+    asyncio.run(main())
